9a154a8d270ed99585b718b5ee922fc3b12f9973
blackjack238 <118196030+blackjack238@users.noreply.github.com>
2023-05-31 15:01:57 +0300

    добавив метод ручної добавлення даних в БД за допомогою Динамічного сету параметрів

--- src/main/java/org/example/DatabasePopulateService.java
+++ src/main/java/org/example/DatabasePopulateService.java
@@ -3,19 +3,16 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabasePopulateService {
+   static Connection conn = new Database("jdbc:postgresql://localhost:32768/modul1","postgres","1111").getINSTANCE().getConnection();
     public static void main(String[] args) {
         // Зчитуємо файл sql/init_db.sql
         String sqlFile = "./src/main/java/sql/populate_db.sql";
         String sqlContent = readSqlFile(sqlFile);
 
         // Отримуємо з'єднання з БД
-        Connection conn = new Database("jdbc:postgresql://localhost:32768/modul1","postgres","1111").getINSTANCE().getConnection();
 
         // Виконуємо запити з файлу sql/init_db.sql
         executeSqlQueries(conn, sqlContent);
@@ -52,6 +49,58 @@ public class DatabasePopulateService {
                     statement.execute();
                 }
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void insertWorker(String name,String data,String level,int salary){
+    String insertWorkersQuery = "INSERT INTO worker (NAME, BIRTHDAY, LEVEL, SALARY) VALUES (?, ?, ?, ?)";
+try (
+     PreparedStatement statement = conn.prepareStatement(insertWorkersQuery)) {
+        statement.setString(1, name);
+        statement.setDate(2, Date.valueOf(data));
+        statement.setString(3, level);
+        statement.setInt(4, salary);
+        statement.executeUpdate();
+
+        // Додаткові вставки працівників з параметрами
+        // ...
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}
+    public static void insertClient(String name){
+        String insertClientsQuery = "INSERT INTO client (NAME) VALUES (?)";
+        try (
+                PreparedStatement statement =conn.prepareStatement(insertClientsQuery)) {
+            statement.setString(1, name);
+            statement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void insertProjects(int ClientId,String StartData,String FinishData){
+        String insertProjectsQuery = "INSERT INTO project (CLIENT_ID, START_DATE, FINISH_DATE) VALUES (?, ?, ?)";
+        try (
+                PreparedStatement statement = conn.prepareStatement(insertProjectsQuery)) {
+            statement.setInt(1, ClientId);
+            statement.setDate(2, Date.valueOf(StartData));
+            statement.setDate(3, Date.valueOf(FinishData));
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void insertProjectWorker(int projectId,int workerId){
+        String insertProjectWorkersQuery = "INSERT INTO project_worker (PROJECT_ID, WORKER_ID) VALUES (?, ?)";
+        try (
+                PreparedStatement statement = conn.prepareStatement(insertProjectWorkersQuery)) {
+            statement.setInt(1, projectId);
+            statement.setInt(2, workerId);
+            statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
